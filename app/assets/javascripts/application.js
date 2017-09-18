78bb522024f578a16c965b4627b01a7445617228
alejovelez10 <alejovelez10@gmail.com>
2017-09-18 12:25:15 -0500

    arreglando scroll

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,6 +21,6 @@
 
 
 $(document).on('turbolinks:load',function() {
-	 setTimeout(function(){ $('#scrollbar1').tinyscrollbar();}, 1000);
+	 setTimeout(function(){ $('#scrollbar1').tinyscrollbar();}, 500);
 
 });
