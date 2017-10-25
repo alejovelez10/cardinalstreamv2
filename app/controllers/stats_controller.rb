538efbdcec07bd9cf5608266d9d57cb6fda86d7d
alejovelez10 <alejovelez10@gmail.com>
2017-10-25 00:28:28 -0500

    estadisticas

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -6,7 +6,6 @@ class StatsController < ApplicationController
 
   	@event = Event.find(params[:id])
   	date = DateTime.now
-  	datet = date - (5*3600)
   	@stat = Stat.new(time_stat: date, type_stat: 0, event_id: @event.id, account_id: @event.account_id , event_name: @event.name, day: date.day, month: date.month, year: date.year, hour: date.hour)
   	@stat.save
 
