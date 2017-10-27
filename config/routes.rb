5741f6a415d02b81f6379c3556ddfcf2b29985d7
alejovelez10 <alejovelez10@gmail.com>
2017-10-27 17:32:02 -0500

    Organizando rutas por defecto

--- config/routes.rb
+++ config/routes.rb
@@ -40,6 +40,8 @@ Rails.application.routes.draw do
   get 'redir', to: 'home#redir', as: 'redir'
   post 'create_question', to: 'questions#create_question', as: 'create_question'
   get 'iframe/:iframe', to:"accounts#iframe", as: "iframe"
+
+  get '*path' => redirect('/portal')
 end
 
 
