c41bfaa24bdc3eea2ab539bfb3d20681b6b1157c
alejovelez10 <alejovelez10@gmail.com>
2017-11-29 10:58:25 -0500

    stats2

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -83,7 +83,7 @@ class AccountsController < ApplicationController
     @sync =  @event.sync
     end
     @nav =  request.user_agent
-    #date = DateTime.now
+    date = DateTime.now
     #views = @event.views != nil ? (@event.views + 1) : 1
     #@event.update(views: views)
     Stat.create(admin_user: @event.admin_user, time_stat: date, type_stat: 0, event_id: @event.id, account_id: @event.account_id , event_name: @event.name, day: date.day, month: date.month, year: date.year, hour: date.hour, minute: date.minute, second: date.second)
