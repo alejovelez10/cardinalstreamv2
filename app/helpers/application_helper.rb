595a9cd1b35e108c4550817ef23155322bdc0647
alejovelez10 <alejovelez10@gmail.com>
2018-02-13 19:08:34 -0500

    organizando chat

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -102,7 +102,7 @@ if fecha != nil
     dm = fecha.strftime("%d") #Dia del mes
     m = fecha.strftime("%m") # Mes del Año
     meses = {"01" => "Enero", "02" => "Febrero","03"=>"Marzo","04" => "Abril", "05" => "Mayo","06"=> "Junio" ,"07"=> "Julio", "08" => "Agosto", "09"=> "Septiembre" ,"10"=> "Octubre","11" => "Noviembre" ,"12" => "Diciembre" }
-    dias = {"1" => "Domingo", "2" => "Lunes","3"=>"Martes","4" => "Miercoles", "5" => "Jueves","6"=> "Viernes" ,"0" =>"Sabado"}
+    dias = {"7" => "Domingo", "1" => "Lunes","2"=>"Martes","3" => "Miercoles", "4" => "Jueves","5"=> "Viernes" ,"6" =>"Sabado"}
     fecha.min < 10 ? min = "0" : min = ""
     fecha.hour < 10 ? hora = "0" : hora = ""
     
