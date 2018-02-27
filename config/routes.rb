f83dee913bf11c588e33411202c57a8b9d6565f6
alejovelez10 <alejovelez10@gmail.com>
2018-02-26 22:26:58 -0500

    loader io

--- config/routes.rb
+++ config/routes.rb
@@ -72,6 +72,7 @@ end
   post 'create_question', to: 'questions#create_question', as: 'create_question'
   get 'iframe/:iframe', to:"accounts#iframe", as: "iframe"
   get '/:name', to: "accounts#portal_show_name"
+  get 'loaderio-919cbd5ee3cf60db728613352b761286.txt'
   get '*path' => redirect('/portal')
 end
 
