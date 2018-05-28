7456e4ecdbc89f961d05c9cb17ac7340f2f948ff
alejovelez10 <alejovelez10@gmail.com>
2018-05-28 13:15:37 -0500

    organizando error

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,10 +5,10 @@ class HomeController < ApplicationController
    @events_o = @account1.events.where(state: 4)
    @events_p = @account1.events.where(state: 0)
    @events_l = @account1.events.where(state: 1)
-   @events_plc = @account1.events.where(state: 1).or(Event.where(state: 0)).count
-   @events_pl = @account1.events.where(state: 1).or(Event.where(state: 0))
-   @events_first = @account1.events.where(state: 1).or(Event.where(state: 0)).order(event_date: :asc).order(state: :desc).first
-   @events_seconds = @account1.events.where(state: 1).or(Event.where(state: 0)).where("id != ?", @events_first.id).order(event_date: :asc).order(state: :desc)
+   @events_plc = @account1.events.where(state: 1).or(@account1.events.where(state: 0)).count
+   @events_pl = @account1.events.where(state: 1).or(@account1.events.where(state: 0))
+   @events_first = @account1.events.where(state: 1).or(@account1.events.where(state: 0)).order(event_date: :asc).order(state: :desc).first
+   @events_seconds = @account1.events.where(state: 1).or(@account1.events.where(state: 0)).where("id != ?", @events_first.id).order(event_date: :asc).order(state: :desc)
 end
 
   def cuenta
